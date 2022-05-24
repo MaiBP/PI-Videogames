c65e71ba546500ffa24509330207b8fc49469a76
Maira Pajaro <maira_pajaro@hotmail.com>
2022-05-24 12:11:11 +0200

    Update About.jsx

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -19,7 +19,6 @@ const About = () => {
             <span>Maira Pajaro</span>
             <span>Full Stack Developer</span>
           </h2>
-      
         <div className={Style.description}>
               <h4>Throughout my life and work career I've been involved in areas of development, teamwork and creativity. I was always curious, ambitious and determined to find solutions to the challenges that presented to me.</h4>
               <h4>A year ago, I decided that needed to focouse in a new career path, and started to explore areas such as the tech industry. I studied by myself every day for a few months and then I got accepted at <a rel='noreferrer' href='https://www.soyhenry.com/' target='_blank'>Soy Henry</a>Bootcamp. I now have a great experience in marketing, art, and all the new technologies I recently incorporated.</h4>
@@ -41,7 +40,6 @@ const About = () => {
                     <span className={Style.buttonTop}>BACK HOME</span></button>
                     </NavLink>
               </div>
-
     </div>
     </div>
     )
